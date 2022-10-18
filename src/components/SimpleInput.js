eaeb2133d5b787df3dadd0f220fd143f45194ddd
Emmanuel <ejaramilo@hotmail.com>
2022-10-18 14:57:33 -0500

    200 dealing with form submit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    // we could use useRef hook in case we might want to handle focus
+    // or because we are going to validate only after submit
+    // it is not good idea to do something like
+
+    // nameInputRef.current.value = ''; BAD IDEA, DO NOT MANIPULATE THE DOM
+    setEnteredName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputChangeHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
