5539849b6c0dda48726bd4e543eec9826821c2b6
Emmanuel <ejaramilo@hotmail.com>
2022-10-31 14:04:30 -0500

    206 managing the overall form validity cleanup and state deriving

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,20 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  const [formIsValid, setFormIsValid] = useState(false);
   
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
-  useEffect(() => {
-    if (enteredNameIsValid) {
-      setFormIsValid(true);
-    } else {
-      setFormIsValid(false);
-    }
-  }, [enteredNameIsValid]);
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  } 
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
