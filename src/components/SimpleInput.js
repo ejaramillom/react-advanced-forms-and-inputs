799ec0f5e49bc9c19a2c04b246d8cab5cae804b6
Emmanuel <ejaramilo@hotmail.com>
2022-10-31 14:01:48 -0500

    206 1 managing the overall form validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-
+  const [formIsValid, setFormIsValid] = useState(false);
+  
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -53,7 +62,7 @@ const SimpleInput = (props) => {
         <p className="error-text">Name must not be empty.</p>
       )}
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
